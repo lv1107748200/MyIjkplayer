f868733bb2752b0eb4e1e309ecd0f58087189ff4
lv <1107748200@qq.com>
2018-05-15 14:06:38 +0800

    例行上传

--- app/src/main/java/com/hr/myijkplayer/ControlPlayer.java
+++ app/src/main/java/com/hr/myijkplayer/ControlPlayer.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.Surface;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.hr.mylibrary.playerview.CusTomSurfaceView;
@@ -162,6 +163,11 @@ public class ControlPlayer extends FrameLayout implements
 
     private void init(){
         cusTomSurfaceView =  CusTomSurfaceView.addSurfaceView(getContext(),this,0,this,this);
+
+        View view =  View.inflate(getContext(), R.layout.video_control_layout_ijk,this);
+
+
+
     }
 
     private void onCreatPlayerView(String url){
@@ -251,8 +257,8 @@ public class ControlPlayer extends FrameLayout implements
             ijkMediaPlayer = null;
 
             IjkMediaPlayer.native_profileEnd();
-
         }
+
     }
 
 
--- app/src/main/java/com/hr/myijkplayer/MainActivity.java
+++ app/src/main/java/com/hr/myijkplayer/MainActivity.java
@@ -3,7 +3,10 @@ package com.hr.myijkplayer;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+
+import com.orhanobut.logger.Logger;
 
 import static com.hr.mylibrary.utils.GSYVideoType.SCREEN_MATCH_FULL;
 
@@ -41,6 +44,80 @@ public class MainActivity extends Activity {
 
                 break;
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        //    NLog.d(NLog.TAGOther,"keyCode--->" + keyCode);
+
+        switch (keyCode) {
+
+            case KeyEvent.KEYCODE_ENTER:     //确定键enter
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+                Logger.d("enter--->");
+
+                controlPlayer.playerOrPause();//遥控确定键
+
+
+                break;
+
+            case KeyEvent.KEYCODE_BACK:    //返回键
+                Logger.d("back--->");
+
+
+                break;
+            case KeyEvent.KEYCODE_MENU:
+                Logger.d("MENU--->");
+
+
+
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_DOWN:   //向下键
+
+                /*    实际开发中有时候会触发两次，所以要判断一下按下时触发 ，松开按键时不触发
+                 *    exp:KeyEvent.ACTION_UP
+                 */
+                Logger.d("KEYCODE_DPAD_DOWN--->");
+                if (event.getAction() == KeyEvent.ACTION_DOWN){
+
+                }
+
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_UP:   //向上键
+                Logger.d("KEYCODE_DPAD_UP--->");
+
+                break;
+
+            case     KeyEvent.KEYCODE_0:   //数字键0
+                Logger.d("KEYCODE_0--->");
+                finish();
+
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_LEFT: //向左键
+
+                //  NLog.e(NLog.TAGOther,"lonKeyDown eft--->" +  event.getRepeatCount());
+
+
+
+
+
+                break;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:  //向右键
+                //   NLog.e(NLog.TAGOther,"onKeyDown right---> " + event.getRepeatCount());
+
+
+
+                break;
+            default:
+                break;
+        }
+
+        return super.onKeyDown(keyCode, event);
+
     }
 
 
